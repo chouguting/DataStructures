d08fd38c8d87a23403fb6bbb48bcc9bf3fd9ea8a
chouguting <chouguting@gmail.com>
2020-09-24 14:26:38 +0800

    added 2-1

---
+++ cmake-build-debug/CMakeFiles/practice2-1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/practice2-1.dir/week2/practice2-1.cpp.obj"
+  "libpractice2-1.dll.a"
+  "practice2-1.exe"
+  "practice2-1.exe.manifest"
+  "practice2-1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/practice2-1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
